4d5e16d00d3555c16df891462d4e11f0883c296d
Filip Brcic <brcha@yandex.com>
2024-03-25 13:17:13 +0100

    feat: implement parsing for recurrence

--- todotxt/src/todorecurrence.rs
+++ todotxt/src/todorecurrence.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum TodoRecurrence {
     Daily(u16, bool),
@@ -5,4 +7,68 @@ pub enum TodoRecurrence {
     Weekly(u16, bool),
     Monthly(u16, bool),
     Yearly(u16, bool),
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TodoRecurrenceParseError;
+
+impl FromStr for TodoRecurrence {
+    type Err = TodoRecurrenceParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let strict_recurrence = s.starts_with('+');
+        let count = if s.len() - (strict_recurrence as usize) == 1 {
+            1
+        } else {
+            s[strict_recurrence as usize..s.len() - 1]
+                .parse::<u16>()
+                .or(Err(TodoRecurrenceParseError))?
+        };
+
+        match s.as_bytes()[s.len() - 1] {
+            b'd' => Ok(TodoRecurrence::Daily(count, strict_recurrence)),
+            b'b' => Ok(TodoRecurrence::BusinessDay(count, strict_recurrence)),
+            b'w' => Ok(TodoRecurrence::Weekly(count, strict_recurrence)),
+            b'm' => Ok(TodoRecurrence::Monthly(count, strict_recurrence)),
+            b'y' => Ok(TodoRecurrence::Yearly(count, strict_recurrence)),
+            _ => Err(TodoRecurrenceParseError),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_test() {
+        assert_eq!(
+            Ok(TodoRecurrence::Daily(3, false)),
+            TodoRecurrence::from_str("3d")
+        );
+        assert_eq!(
+            Ok(TodoRecurrence::Daily(17, true)),
+            TodoRecurrence::from_str("+17d")
+        );
+        assert_eq!(
+            Ok(TodoRecurrence::BusinessDay(8, false)),
+            TodoRecurrence::from_str("8b")
+        );
+        assert_eq!(
+            Ok(TodoRecurrence::Weekly(4, true)),
+            TodoRecurrence::from_str("+4w")
+        );
+        assert_eq!(
+            Ok(TodoRecurrence::Monthly(1, false)),
+            TodoRecurrence::from_str("m")
+        );
+        assert_eq!(
+            Ok(TodoRecurrence::Yearly(1, true)),
+            TodoRecurrence::from_str("+y")
+        );
+        assert_eq!(
+            TodoRecurrence::from_str("d"),
+            TodoRecurrence::from_str("1d")
+        );
+    }
 }
