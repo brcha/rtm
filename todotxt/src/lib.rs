e61d6536daa90aac9a813062fe69cd966cadfbe3
Filip Brcic <brcha@yandex.com>
2025-09-15 23:28:06 +0200

    feat: remove cargo init code from lib.rs

--- todotxt/src/lib.rs
+++ todotxt/src/lib.rs
@@ -1,20 +1,5 @@
+pub mod todocontext;
 pub mod todoitem;
 pub mod todopriority;
-pub mod todorecurrence;
 pub mod todoproject;
-pub mod todocontext;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod todorecurrence;
